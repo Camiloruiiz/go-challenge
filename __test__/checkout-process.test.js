4a14b54791f0b1bf673fbbbb6096df39fcefb4e2
Camiloruiiz <camiloruiz@esquemacreativo.com>
2018-09-29 13:53:34 +0100

    [GREEN]

--- __test__/checkout-process.test.js
+++ __test__/checkout-process.test.js
@@ -1,3 +1,5 @@
+import {priceList} from '../index.js'
+
 describe('Supermarket checkout process',()=> {
   test('Product A value', ()=> {
     expect(priceList['A']).toBe(50);
---
+++ index.js
@@ -0,0 +1,3 @@
+export const priceList = {
+  A: 50,
+}
