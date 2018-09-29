6f4509e3b194f081085db46af4bce78fecf22a33
Camiloruiiz <camiloruiz@esquemacreativo.com>
2018-09-29 19:11:41 +0100

    [GREEN]

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ export function getProductValue(product) {
 
 export function sumValues(shoppingList) {
   let listByProduct = shoppingList.split('')
+  let listGroupByProduct = {}
+  
   let totalValue = 0
 
+  listByProduct.forEach(element => {
+    if (listGroupByProduct[element]){
+      listGroupByProduct[element] = listGroupByProduct[element] + element
+    } else {
+      listGroupByProduct[element] = element
+    }
+  })
+  
   listByProduct.forEach(element => {
     totalValue += getProductValue(element)
   })
@@ -22,5 +32,11 @@ export function sumValues(shoppingList) {
     return 'Shopping list is empty.'
   }
 
+  if(listGroupByProduct['A']){
+    let amoutnAProducts = listGroupByProduct['A'].length
+    let totalASavings = Math.floor(amoutnAProducts/3)*20
+    return totalValue - totalASavings
+  }
+  
   return totalValue
 }
