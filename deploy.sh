b6af34b38e7b632f9b06e049b871fc5c40471bd4
Camiloruiiz <camiloruiz@esquemacreativo.com>
2019-04-23 21:35:02 +0100

    [implement] docs folder

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,19 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+cd docs
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'docs'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Camiloruiiz/go-compare-challenge.git master:gh-pages
+git push -f git@github.com:Camiloruiiz/go-challenge.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/go-compare-challenge/'
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/go-challenge/'
     : '/'
 }
